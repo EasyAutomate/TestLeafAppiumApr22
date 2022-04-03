ce0b436e605a50191d1f25db5da0fd9c05586f27
Thyagarajan <lokeshkumar.thyagarajan@hidglobal.com>
2022-04-03 18:54:58 +0530

    Day 2 code

---
+++ src/main/java/learning/FirstCodeInAndroid.java
@@ -0,0 +1,27 @@
+package learning;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.AppiumDriver;
+
+public class FirstCodeInAndroid {
+	
+	public static void main(String[] args) throws MalformedURLException {
+		DesiredCapabilities dc = new DesiredCapabilities();
+		dc.setCapability("platformName", "Android");
+		dc.setCapability("deviceName", "Lokesh device");
+		//dc.setCapability("app", System.getProperty("user.dir")+"/apks/VodQA.apk");
+		dc.setCapability("appPackage", "com.vodqareactnative");
+		dc.setCapability("appActivity", "com.vodqareactnative.MainActivity");
+		dc.setCapability("noReset", true);
+		AppiumDriver driver = new AppiumDriver(new URL("http://0.0.0.0:4723/wd/hub"), dc);
+		driver.findElement(By.xpath("//*[@text='LOG IN']")).click();
+		driver.quit();
+		
+	}
+
+}
