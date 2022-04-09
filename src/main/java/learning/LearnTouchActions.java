4d0093c0cbef564c8c456ef6740fd55081578aed
Lokesh Kumar Thyagarajan <LokeshKumar.Thyagarajan@hidglobal.com>
2022-04-09 19:01:08 +0530

    Day 3 code

--- src/main/java/learning/LearnTouchActions.java
+++ src/main/java/learning/LearnTouchActions.java
@@ -10,14 +10,14 @@ import org.openqa.selenium.interactions.PointerInput.Kind;
 import org.openqa.selenium.interactions.PointerInput.MouseButton;
 import org.openqa.selenium.interactions.PointerInput.Origin;
 import org.openqa.selenium.interactions.Sequence;
-import org.openqa.selenium.interactions.touch.TouchActions;
+//import org.openqa.selenium.interactions.touch.TouchActions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MultiTouchAction;
-import io.appium.java_client.TouchAction;
-import io.appium.java_client.touch.WaitOptions;
-import io.appium.java_client.touch.offset.PointOption;
+//import io.appium.java_client.MultiTouchAction;
+//import io.appium.java_client.TouchAction;
+//import io.appium.java_client.touch.WaitOptions;
+//import io.appium.java_client.touch.offset.PointOption;
 
 public class LearnTouchActions {
 	public static void main(String[] args) throws MalformedURLException, InterruptedException {
@@ -29,7 +29,9 @@ public class LearnTouchActions {
 		AppiumDriver driver = new AppiumDriver(new URL("http://0.0.0.0:4723/wd/hub"), dc);
 		int maxY = driver.manage().window().getSize().getHeight();
 		int maxX = driver.manage().window().getSize().getWidth();
-		Thread.sleep(5000);
+		//Below code will not work for Appium version 8.X.Y and above
+		//Downgrade appium version to 7.X.Y for below methods (TouchActions and MultiTouchAction) to work
+		/*
 		// Swipe up
 		new TouchAction<>(driver).press(PointOption.point((int) (maxX * 0.5), (int) (maxY * 0.8)))
 				.waitAction(WaitOptions.waitOptions(Duration.ofMillis(2000)))
@@ -54,22 +56,31 @@ public class LearnTouchActions {
 				.add(new TouchAction<>(driver).press(PointOption.point((int) (maxX * 0.5), (int) (maxY * 0.2)))
 						.waitAction(WaitOptions.waitOptions(Duration.ofMillis(2000)))
 						.moveTo(PointOption.point((int) (maxX * 0.5), (int) (maxY * 0.8))).release())
-				.perform();
+				.perform();			
+		*/		
 		//Pointer Input
+		//Pinch
 		PointerInput input = new PointerInput(Kind.TOUCH, "finger1");
-		Sequence swipeUp = new Sequence(input, 1);
-		swipeUp.addAction(input.createPointerMove(Duration.ofMillis(0), Origin.viewport(), (int) (maxX * 0.5), (int) (maxY * 0.8)));
-		swipeUp.addAction(input.createPointerDown(MouseButton.LEFT.asArg()));
-		swipeUp.addAction(input.createPointerMove(Duration.ofMillis(2000), Origin.viewport(), (int) (maxX * 0.5), (int) (maxY * 0.2)));
-		swipeUp.addAction(input.createPointerUp(MouseButton.LEFT.asArg()));
+		Sequence finger1 = new Sequence(input, 1);
+		finger1.addAction(input.createPointerMove(Duration.ofMillis(0), Origin.viewport(), (int) (maxX * 0.25), (int) (maxY * 0.25)));
+		finger1.addAction(input.createPointerDown(MouseButton.LEFT.asArg()));
+		finger1.addAction(input.createPointerMove(Duration.ofMillis(2000), Origin.viewport(), (int) (maxX * 0.5), (int) (maxY * 0.5)));
+		finger1.addAction(input.createPointerUp(MouseButton.LEFT.asArg()));
 		PointerInput input2 = new PointerInput(Kind.TOUCH, "finger2");
-		Sequence swipeDown = new Sequence(input2, 1);
-		swipeDown.addAction(input2.createPointerMove(Duration.ofMillis(0), Origin.viewport(), (int) (maxX * 0.5), (int) (maxY * 0.2)));
-		swipeDown.addAction(input2.createPointerDown(MouseButton.LEFT.asArg()));
-		swipeDown.addAction(input2.createPointerMove(Duration.ofMillis(2000), Origin.viewport(), (int) (maxX * 0.5), (int) (maxY * 0.8)));
-		swipeDown.addAction(input2.createPointerUp(MouseButton.LEFT.asArg()));
-		driver.perform(Arrays.asList(swipeUp,swipeDown));
-		
+		Sequence finger2 = new Sequence(input2, 1);
+		finger2.addAction(input2.createPointerMove(Duration.ofMillis(0), Origin.viewport(), (int) (maxX * 0.75), (int) (maxY * 0.75)));
+		finger2.addAction(input2.createPointerDown(MouseButton.LEFT.asArg()));
+		finger2.addAction(input2.createPointerMove(Duration.ofMillis(2000), Origin.viewport(), (int) (maxX * 0.5), (int) (maxY * 0.5)));
+		finger2.addAction(input2.createPointerUp(MouseButton.LEFT.asArg()));
+		driver.perform(Arrays.asList(finger1,finger2));
+		//Swipe up 
+		PointerInput input3 = new PointerInput(Kind.TOUCH, "finger1");
+		Sequence swipeUp1 = new Sequence(input3, 1);
+		swipeUp1.addAction(input3.createPointerMove(Duration.ofMillis(0), Origin.viewport(), (int) (maxX * 0.5), (int) (maxY * 0.8)));
+		swipeUp1.addAction(input3.createPointerDown(MouseButton.LEFT.asArg()));
+		swipeUp1.addAction(input3.createPointerMove(Duration.ofMillis(2000), Origin.viewport(), (int) (maxX * 0.5), (int) (maxY * 0.2)));
+		swipeUp1.addAction(input3.createPointerUp(MouseButton.LEFT.asArg()));
+		driver.perform(Arrays.asList(swipeUp1));
 
 	}
 }
---
+++ src/main/java/learning/LearnTouchActionsWithinWebElement.java
@@ -0,0 +1,58 @@
+package learning;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.Arrays;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.PointerInput.Kind;
+import org.openqa.selenium.interactions.PointerInput.MouseButton;
+import org.openqa.selenium.interactions.PointerInput.Origin;
+import org.openqa.selenium.interactions.Sequence;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.AppiumDriver;
+
+public class LearnTouchActionsWithinWebElement {
+	public static void main(String[] args) throws MalformedURLException, InterruptedException {
+		DesiredCapabilities dc = new DesiredCapabilities();
+		dc.setCapability("platformName", "Android");
+		dc.setCapability("deviceName", "Lokesh device");
+		dc.setCapability("appPackage", "com.android.vending");
+		dc.setCapability("appActivity", "com.android.vending.AssetBrowserActivity");
+		dc.setCapability("noReset", true);
+		AppiumDriver driver = new AppiumDriver(new URL("http://0.0.0.0:4723/wd/hub"), dc);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		WebElement element = driver.findElement(By.xpath(
+				"//android.view.View[@content-desc='More results for Welcome to Google Play']/../following-sibling::android.view.View"));
+		String bounds = element.getAttribute("bounds");
+		String[] split = bounds.replace("][", "!").replace("[", "").replace("]", "").split("!");
+		System.out.println(split[0]);
+		System.out.println(split[1]);
+		String[] eleStart = split[0].split(",");
+		String[] eleEnd = split[1].split(",");
+		// Swipe left
+		int startX = (int) ((Integer.parseInt(eleEnd[0]) - Integer.parseInt(eleStart[0])) * 0.8)
+				+ Integer.parseInt(eleStart[0]);
+		int startY = (int) ((Integer.parseInt(eleEnd[1]) - Integer.parseInt(eleStart[1])) * 0.5)
+				+ Integer.parseInt(eleStart[1]);
+		int endX = (int) ((Integer.parseInt(eleEnd[0]) - Integer.parseInt(eleStart[0])) * 0.2)
+				+ Integer.parseInt(eleStart[0]);
+		int endY = (int) ((Integer.parseInt(eleEnd[1]) - Integer.parseInt(eleStart[1])) * 0.5)
+				+ Integer.parseInt(eleStart[1]);
+
+		PointerInput input3 = new PointerInput(Kind.TOUCH, "finger1");
+		Sequence swipeLeft = new Sequence(input3, 1);
+		swipeLeft.addAction(input3.createPointerMove(Duration.ofMillis(0), Origin.viewport(), startX, startY));
+		swipeLeft.addAction(input3.createPointerDown(MouseButton.LEFT.asArg()));
+		swipeLeft.addAction(input3.createPointerMove(Duration.ofMillis(2000), Origin.viewport(), endX, endY));
+		swipeLeft.addAction(input3.createPointerUp(MouseButton.LEFT.asArg()));
+		driver.perform(Arrays.asList(swipeLeft));
+		driver.perform(Arrays.asList(swipeLeft));
+		driver.perform(Arrays.asList(swipeLeft));
+
+	}
+}
